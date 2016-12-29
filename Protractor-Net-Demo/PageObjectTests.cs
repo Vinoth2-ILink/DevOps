debb778a867b932cb130a3b5e86bf8f5c3a5082d
Vinoth-ILink <vinothp@ilink-systems.com>
2016-12-29 19:06:50 +0530

    driver quit update

--- Protractor-Net-Demo/PageObjectTests.cs
+++ Protractor-Net-Demo/PageObjectTests.cs
@@ -21,6 +21,7 @@ namespace Protractor_Net_Demo
             driver = new ChromeDriver();
             driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(10));
             page = new SuperCalculatorPage(driver, URL);
+           
         }
 
         [Test]
@@ -49,7 +50,7 @@ namespace Protractor_Net_Demo
         [TearDown]
         public void Teardown()
         {
-            driver.Quit();
+            //driver.Quit();
         }
     }
 }
